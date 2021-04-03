a2abd39c8ff8164ed84f417533cb698a628230db
Himanshu Gupta <gupta.h@husky.neu.edu>
2021-04-03 00:33:18 -0400

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -1,3 +1,5 @@
+# Complete the function below.
+
 def findZeroSum(arr):
     # Write your code here.
     def helper2_sum(arr, ind_3, target):
@@ -6,8 +8,6 @@ def findZeroSum(arr):
         arr[ind_3], arr[start] = arr[start], arr[ind_3]
         start +=1
         while start <= end:
-            
-
             if arr[start] + arr[end] > target:
                 end -=1
             elif arr[start] + arr[end] < target:
@@ -17,14 +17,11 @@ def findZeroSum(arr):
                nums.sort()
                return (', '.join((str(nums[0]), str(nums[1]), str(nums[2]))))
         return   
-        
-
-        result = []
-        for i in range(len(arr)-1):
-            target = 0-arr[i]
-
-            tmp = helper2_sum(arr, i, target)
-            if tmp:
-                if tmp not in result:
-                    result.append(tmp)
-        return result
+    result = []
+    for i in range(len(arr)-1):
+        target = 0-arr[i]
+        tmp = helper2_sum(arr, i, target)
+        if tmp:
+            if tmp not in result:
+                result.append(tmp)
+    return result
