aee9343704e2567f277daceedebc9437335f649e
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-05-22 22:49:20 -0500

    Update container_with_water.py

--- container_with_water.py
+++ container_with_water.py
@@ -14,3 +14,19 @@ class Solution:
             else:
                 rstart -= 1
         return max_area
+
+  
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        start, end = 0, len(height)-1
+        max_area = 0
+        while start <= end:
+            
+            tmp_area = min(height[start], height[end])*(end - start)
+            if tmp_area > max_area:
+                max_area = tmp_area
+            if height[start] > height[end]:
+                end -= 1
+            else:
+                start += 1
+        return max_area
