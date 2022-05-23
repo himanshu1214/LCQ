b8ff05fdc6a43f3bfb71c08cfe00501a798d4b5e
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-05-23 07:24:30 -0500

    Update climbStairs.py

--- DP/climbStairs.py
+++ DP/climbStairs.py
@@ -17,3 +17,18 @@ class Solution:
             
         hash_map[n] = helper(n)
         return hash_map[n]
+
+    
+    
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #                   n 
+        #           n-1             n-2
+        #       n-2   n-3       n-3     n-4
+        dp = [None for i in range(n+1)]
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
