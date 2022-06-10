ef15359391a4715c60a3b174526a2a6656130b21
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-06-09 21:52:40 -0500

    Update meetingrooms.py

--- Sorting/meetingrooms.py
+++ Sorting/meetingrooms.py
@@ -5,3 +5,20 @@ class Solution:
             if intervals[i][1]> intervals[i+1][0]:
                 return False
         return True
+
+# Try2:
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        start = 1
+        end = len(intervals)
+        
+        if len(intervals) == 1:
+            return True
+        
+        intervals.sort()
+        while start < end:
+            if intervals[start-1][1] > intervals[start][0]:
+                return False
+            start +=1   
+        return True
+                
