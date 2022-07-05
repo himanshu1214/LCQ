afb2cd44002a0342c8b8b5142e3664e58fcafe20
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-07-04 19:33:06 -0500

    Create sumOfSubarrayMin.py

---
+++ Misc/sumOfSubarrayMin.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        A =arr
+        A = [0]+A
+        result = [0]*len(A)
+        stack = [0]
+        for i in range(len(A)):
+            while A[stack[-1]] > A[i]:
+                stack.pop() 
+            j = stack[-1]
+            result[i] = result[j] + (i-j)*A[i]
+            stack.append(i)
+        return sum(result) % (10**9+7)
+                
