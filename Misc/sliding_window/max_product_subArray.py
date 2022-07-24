e7e8c2b3105b0d37e7821808fc79d5809df77984
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-07-24 12:34:14 -0500

    Update max_product_subArray.py

--- Misc/sliding_window/max_product_subArray.py
+++ Misc/sliding_window/max_product_subArray.py
@@ -1,5 +1,23 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        
+        res = max(nums)
+        curr_max,  curr_min = 1, 1
+        
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                curr_max, curr_min = 1, 1
+                continue
+            tmp = nums[i] * curr_max
+            curr_max = max(curr_max * nums[i], nums[i], curr_min * nums[i])
+            curr_min = min(tmp, nums[i], curr_min * nums[i])
+
+            res = max(res, curr_max)
+            
+        return res
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
         prod = nums[0]
         tmp = nums[0]
         curr_max, curr_min =  nums[0], nums[0]
