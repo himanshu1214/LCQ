28716ceb7120b4758711337d75a935047af20d3a
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-01 00:07:19 -0500

    Update longest_consequtive.py

--- Misc/longest_consequtive.py
+++ Misc/longest_consequtive.py
@@ -1,3 +1,4 @@
+# Brute force
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         longest_streak = 0 
@@ -11,4 +12,30 @@ class Solution:
                 
             longest_streak = max(longest_streak, current_streak)
             
+        return longest_streak
+
+
+# Sorting
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        nums.sort()
+        
+        print(nums)
+        current_streak = 1
+        longest_streak = 0
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                continue
+            elif nums[i] == nums[i-1] +1:
+                current_streak += 1
+            
+            else:
+                longest_streak = max(longest_streak, current_streak)
+                current_streak = 1
+        longest_streak = max(longest_streak, current_streak)          
+        
         return longest_streak
