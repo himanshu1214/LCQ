375259faf9ebe69b5b171ed5f3a7f53b47db1a65
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-04 00:11:45 -0500

    Update longest_repeating_character_replacement.py

--- Misc/sliding_window/longest_repeating_character_replacement.py
+++ Misc/sliding_window/longest_repeating_character_replacement.py
@@ -13,3 +13,25 @@ class Solution:
             res = max(res, (right - left +1))  
             right += 1
         return res
+
+    
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        from collections import defaultdict
+        hash_tab = defaultdict(int)
+        left = 0
+        right = 0
+        max_count = 0
+        max_window_len = 0
+        res = 0
+        while left <= right and right < len(s):
+            hash_tab[s[right]] += 1
+            
+            max_count = max(max_count,hash_tab[s[right]])
+            
+            while (right - left + 1) - max_count > k:
+                hash_tab[s[left]] -= 1
+                left += 1
+            res = max(res, (right - left +1))  
+            right += 1
+        return res
