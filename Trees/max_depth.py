46cfc42b8e4314d619bd90316c32958de9b24f90
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-06 00:55:43 -0500

    Update max_depth.py

--- Trees/max_depth.py
+++ Trees/max_depth.py
@@ -39,3 +39,28 @@ class Solution:
             return max(left_depth, right_depth)
             
         return get_depth(root)
+
+# Alternate
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+        global_depth = [0]
+        def dfs(root, curr_depth):
+            if not root:
+                if curr_depth > global_depth[0]:
+                    global_depth[0] = curr_depth
+                    
+                return 
+                    
+            dfs(root.left, curr_depth +1)
+            dfs(root.right, curr_depth +1)
+            
+            
+        dfs(root, 0)
+        return global_depth[0]
