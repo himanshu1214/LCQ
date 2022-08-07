8e199bafbdbd2288795722a70785b205f5f499d6
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-07 15:37:23 -0500

    Update serialization_deserialization_btree.py

--- Trees/serialization_deserialization_btree.py
+++ Trees/serialization_deserialization_btree.py
@@ -55,3 +55,60 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+
+# Alternate
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    def __init__(self):
+        self.index = 0
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = []
+        def dfs(root):
+            if not root:
+                res.append("null")
+                return 
+            
+            res.append(str(root.val))
+            dfs(root.left)
+        
+            dfs(root.right)
+            
+            
+        dfs(root)
+        return ",".join(res)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = data.split(",")
+        self.index = 0
+        def dfs():
+            if data[self.index] == 'null':
+                self.index += 1
+                return 
+            
+            node = TreeNode(int(data[self.index]))
+            self.index += 1 
+            node.left = dfs()
+            node.right = dfs()
+            
+            return node
+        return dfs()
