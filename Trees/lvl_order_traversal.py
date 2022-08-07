f9dc482b6e00faaaa0cd5213899adb5af84f6baa
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-07 11:59:43 -0500

    Update lvl_order_traversal.py

--- Trees/lvl_order_traversal.py
+++ Trees/lvl_order_traversal.py
@@ -61,3 +61,40 @@ class Solution:
             return lvl_order
         
         return bfs(root)
+
+# Alternate
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        from queue import Queue
+        if not root:
+            return []
+        
+        def bfs(root):
+            q = Queue()
+            q.put(root)
+            res = []
+            while not q.empty():
+                siz = q.qsize()
+                tmp = []
+                for i in range(siz):
+                    node = q.get()
+                    if node.left:
+                        q.put(node.left)
+
+                    if node.right:
+                        q.put(node.right)
+                        
+                    tmp.append(node.val)
+                    
+                res.append(tmp)
+            return res
+        
+        return bfs(root)
+        
