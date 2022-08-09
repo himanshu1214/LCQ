e969dfa5a63aff8fc85f72e0e05762afc7cd2d0c
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-08 20:48:21 -0500

    Update kthsmallest.py

--- Trees/kthsmallest.py
+++ Trees/kthsmallest.py
@@ -28,3 +28,24 @@ class Solution:
             
         dfs(root)
         return -max_heap[0]
+    
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+        def dfs(root):
+            if not root:
+                return 
+            
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        res.sort()
+        return res[k-1]
