c8845905d38883c9ee6f23f6ddc284b35bfcc3f9
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-09 00:11:00 -0500

    Update prefix_tree.py

--- Trees/prefix_tree.py
+++ Trees/prefix_tree.py
@@ -33,3 +33,42 @@ class Trie:
             else:
                 return False
         return True
+
+    
+class TrieNode:
+    def __init__(self):
+        self.word =  False
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        print(node.children)
+        for i in word:
+            if i not in node.children:
+                node.children[i] = TrieNode()
+            node = node.children[i]
+        node.word = True
+                
+    def search(self, word: str) -> bool:
+        node = self.root
+        for i in word:
+            if i in node.children:
+                node = node.children[i]
+            else:
+                return False
+        return node.word
+            
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for i in prefix:
+            if i in node.children:
+                node = node.children[i]
+            else:
+                return False
+        return True
