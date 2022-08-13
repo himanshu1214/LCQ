5ad3e1ea820d9aa1cbd0aed85b937e6d8e985e02
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-08-12 22:02:16 -0500

    Update canConstruct.py

--- Misc/canConstruct.py
+++ Misc/canConstruct.py
@@ -17,3 +17,25 @@ class Solution:
                 return False
             
         return True
+
+    
+# Alternate
+from collections import defaultdict
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ran_dic = [0 for i in range(26)]
+        
+        mag_dict = [0 for j in range(26)]
+        
+        for i in ransomNote:
+            ran_dic[int(ord(i.upper()) - 65)] += 1
+            
+        for j in magazine:
+            mag_dict[int(ord(j.upper())) - 65] += 1
+            
+        
+        for k in range(len(ran_dic)):
+            if ran_dic[k] and mag_dict[k] < ran_dic[k]: 
+                return False
+            
+        return True
