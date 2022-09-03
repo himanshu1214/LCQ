ac46916943193d2e57ae5718d9a54cbaca5c0e9c
Himanshu Gupta <himanshu90gupta@outlook.com>
2022-09-03 16:37:52 -0500

    Update rob_houses.py

--- Recursion/rob_houses.py
+++ Recursion/rob_houses.py
@@ -1,6 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        res = [float('-inf')]
         hash_map = {}
         def dfs(nums, index, money):
             if index> len(nums)-1:
@@ -17,3 +16,21 @@ class Solution:
             
         return dfs(nums, 0, 0)
         # print(hash_map)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        hash_map = {}
+        def dfs(nums, index):
+            if index> len(nums)-1:
+                return 0
+            
+            if index in hash_map:
+                return hash_map[index]
+            
+
+            val = max(dfs(nums, index + 2) + nums[index], dfs(nums, index+1))
+            hash_map[index] = val
+            
+            return val
+            
+        return dfs(nums, 0)
